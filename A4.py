5fbdc5e0dddb2330f1d03d0e01282cda607f57d0
BeamMyUp <myriam.beauvais@csgames.org>
2017-11-10 15:40:10 -0500

    Implement gaussian deblur and non-local means (Q1)

--- A4.py
+++ A4.py
@@ -6,16 +6,19 @@ def main():
     filename = "imgA4/ISO409600_SonyA7SII_lowlight_image.jpg"
 
     img1 = cv.imread(filename)
-    b, g, r = cv.split(img1)
-    img1 = cv.merge([r, g, b])
-    img1 = img1.astype(float)
 
-    cv.normalize(img1, img1, 0, 1, cv.NORM_MINMAX)
+    q1GaussBlur = True
+    q1NLM = False
+
+    if q1GaussBlur:
+        img1 = cv.GaussianBlur(img1, (15, 15), 3)
+    elif q1NLM:
+        img1 = cv.fastNlMeansDenoising(img1, h=17, templateWindowSize=7, searchWindowSize=21)
 
     cv.imshow('dst_rt', img1)
 #
     cv.normalize(img1, img1, 0, 255, cv.NORM_MINMAX)
-    cv.imwrite("framed_greyEdge.jpg", img1)
+    cv.imwrite("q1_gauss_15_3.jpg", img1)
     cv.waitKey(0)
 
 if __name__ == '__main__':
